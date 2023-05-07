f84ec289934770958285c07af61f87fd0f03cd39
ToyosatomiminoMiko <63940347+ToyosatomiminoMiko@users.noreply.github.com>
2023-05-08 00:01:04 +0800

    命令行传参功能添加

--- main.rs
+++ main.rs
@@ -1,12 +1,16 @@
+use std::env;
 use std::fs::File;
-use std::io::{self, Write, Error, Read};
-use std::{thread, time::{self, Duration}};
+use std::{
+    io::{self, Error, Read, Write},
+    thread,
+    time::{self, Duration},
+};
 
 macro_rules! printf {
     /*
     标准输出中的这种行缓冲机制
     在遇到换行符之前，输出的内容并不会隐式的刷新
-    这就导致 print! 宏和 println! 宏实际上并不完全相同 
+    这就导致 print! 宏和 println! 宏实际上并不完全相同
      */
     ($($arg:tt)*) =>{
         print!($($arg)*);
@@ -14,7 +18,7 @@ macro_rules! printf {
     }
 }
 
-fn tnop(s:String, t: Duration){
+fn tnop(s: String, t: Duration) {
     let bytes = s.as_bytes(); // 转换为字节数组
     /*
     使用 iter 方法创建了一个可以遍历字节数组的迭代器。
@@ -34,15 +38,29 @@ fn tnop(s:String, t: Duration){
     }
 }
 
-fn main() -> Result<(), Error>  {
-    let path = "./tno.txt";
-    let mut input = File::open(path).expect("Failed to open.");
+fn string_to_static_str(s:String) -> &'static str {
+    /*
+    将`String`转换为`&str` 
+    */
+    Box::leak(s.into_boxed_str())
+}
+
+/*
+```shell
+cargo run ./tno.txt 250
+```
+*/
+fn main() -> Result<(), Error> {
+    let args: Vec<String> = env::args().collect();
+    //let path: &str = "./tno.txt";
+    let path: &str = string_to_static_str(args[1].to_string());
+    let time = args[2].to_string().parse::<u64>().unwrap();
+    
+    let mut input: File = File::open(path).expect("Failed to open.");
     //let buffered = BufReader::new(input);
-    let mut contents = String::new();
+    let mut contents: String = String::new();
     input.read_to_string(&mut contents).unwrap();
-    let ten_millis = time::Duration::from_millis(50); // ms
+    let ten_millis: Duration = time::Duration::from_millis(time); // ms
     tnop(contents, ten_millis);
     Ok(())
 }
-
-
